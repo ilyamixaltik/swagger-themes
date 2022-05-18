b1a902e6bd8b72b112b4ff11288c5b9f2fd3eca5
ilyamixaltik <mixaltik16@gmail.com>
2022-05-18 20:27:31 +0300

    update swagger theme name type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type SwaggerThemeName = 'classic' | 'dark' | 'outline' | 'newspaper';
+export type SwaggerThemeName = 'classic' | 'dark' | 'outline' | 'newspaper' | 'monokai';
 export type VersionSwagger = 'v2' | 'v3';
 export type DefaultConfig = {
   explorer: boolean,
