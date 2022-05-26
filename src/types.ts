7f39eb1287bb254b156352c058d233b16ccb9388
ilyamixaltik <mixaltik16@gmail.com>
2022-05-26 22:14:06 +0300

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type SwaggerThemeName = 'classic' | 'dark' | 'outline' | 'newspaper' | 'monokai' | 'feeling-blue';
+export type SwaggerThemeName = 'classic' | 'dark' | 'outline' | 'newspaper' | 'monokai' | 'feeling-blue' | 'muted';
 export type VersionSwagger = 'v2' | 'v3';
 export type DefaultConfig = {
   explorer: boolean,
