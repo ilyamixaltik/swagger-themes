d3acaa07684e95e8c97f347c661e50c0f7f21c7e
ilyamixaltik <mixaltik16@gmail.com>
2024-02-11 21:29:18 +0300

    update swagger theme name type

--- src/types/swagger-theme-name.ts
+++ src/types/swagger-theme-name.ts
@@ -3,6 +3,7 @@ import { SwaggerThemeNameEnum } from '../enums/swagger-theme-name';
 export type SwaggerThemeName =
   SwaggerThemeNameEnum.CLASSIC |
   SwaggerThemeNameEnum.DARK |
+  SwaggerThemeNameEnum.DRACULA |
   SwaggerThemeNameEnum.FEELING_BLUE |
   SwaggerThemeNameEnum.FLATTOP |
   SwaggerThemeNameEnum.MATERIAL |
