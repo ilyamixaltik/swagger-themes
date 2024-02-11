d9ebaef9eb0826acc81ae8a113f13de4c465f927
ilyamixaltik <mixaltik16@gmail.com>
2024-02-11 21:29:05 +0300

    update swagger theme name enum

--- src/enums/swagger-theme-name.ts
+++ src/enums/swagger-theme-name.ts
@@ -1,6 +1,7 @@
 export enum SwaggerThemeNameEnum {
   CLASSIC = 'classic',
   DARK = 'dark',
+  DRACULA = 'dracula',
   FEELING_BLUE = 'feeling-blue',
   FLATTOP = 'flattop',
   MATERIAL = 'material',
