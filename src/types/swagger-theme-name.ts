9ab26539513304cff3ef959863e5bc7cc6e7f206
ilyamixaltik <mixaltik16@gmail.com>
2024-02-11 22:07:16 +0300

    fix swagger theme name type

--- src/types/swagger-theme-name.ts
+++ src/types/swagger-theme-name.ts
@@ -2,12 +2,16 @@ import { SwaggerThemeNameEnum } from '../enums/swagger-theme-name';
 
 export type SwaggerThemeName =
   SwaggerThemeNameEnum.CLASSIC |
+  SwaggerThemeNameEnum.DARK_MONOKAI |
   SwaggerThemeNameEnum.DARK |
   SwaggerThemeNameEnum.DRACULA |
   SwaggerThemeNameEnum.FEELING_BLUE |
   SwaggerThemeNameEnum.FLATTOP |
+  SwaggerThemeNameEnum.GRUVBOX |
   SwaggerThemeNameEnum.MATERIAL |
   SwaggerThemeNameEnum.MONOKAI |
   SwaggerThemeNameEnum.MUTED |
   SwaggerThemeNameEnum.NEWSPAPER |
+  SwaggerThemeNameEnum.NORD_DARK |
+  SwaggerThemeNameEnum.ONE_DARK |
   SwaggerThemeNameEnum.OUTLINE;
