a8e2ab28e880b8451f8e5dee39be0613db78f0ea
ilyamixaltik <mixaltik16@gmail.com>
2024-02-11 20:25:16 +0300

    created swagger theme name enum

---
+++ src/enums/swagger-theme-name.ts
@@ -0,0 +1,11 @@
+export enum SwaggerThemeNameEnum {
+  CLASSIC = 'classic',
+  DARK = 'dark',
+  FEELING_BLUE = 'feeling-blue',
+  FLATTOP = 'flattop',
+  MATERIAL = 'material',
+  MONOKAI = 'monokai',
+  MUTED = 'muted',
+  NEWSPAPER = 'newspaper',
+  OUTLINE = 'outline',
+}
